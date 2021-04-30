6647e17418df57f439e8f299d0e9d7146061d1c6
jarencudilla <jarencudilla@gmail.com>
2021-04-30 22:29:43 +0800

    Create square.rb
    Finished square of squares coding challenge

---
+++ square.rb
@@ -0,0 +1,13 @@
+test_arr =[-1, 0, 3, 4, 25, 26]
+
+def isSquare(num)
+    i = 0
+    return false if num < 0
+    (0..num).each do
+        return true if (i*i == num)
+        i += 1
+        return false if (i*i > num)
+    end
+end
+
+test_arr.each {|x| puts "#{x} => #{isSquare(x)}"}
