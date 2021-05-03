9a918673a438dc632bbf87da02f2ba44ea466eb0
jarencudilla <jarencudilla@gmail.com>
2021-05-03 20:36:24 +0800

    Create animal.rb

---
+++ animal.rb
@@ -0,0 +1,11 @@
+class Animal
+    def initialize(name, color, age)
+     @name = name
+     @color = color
+     @age = age
+    end
+    def walk
+     puts "hey #{@name}, let's walk!" 
+    end
+end
+
