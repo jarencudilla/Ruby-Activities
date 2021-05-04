989e3585e353e6a1d0579c40046d4ee3edbadf53
jarencudilla <jarencudilla@gmail.com>
2021-05-04 21:51:50 +0800

    Update rubyactivity2_1.rb
    from
    
    arr = [1, 3, 5, 7, 9, 11]
    number = 5
    arr.each do |num|
        if num == number
          puts "The number #{number} appears in the array"
        end
      end

--- rubyactivity2_1.rb
+++ rubyactivity2_1.rb
@@ -1,8 +1,8 @@
+# frozen_string_literal: true
+
 arr = [1, 3, 5, 7, 9, 11]
 number = 5
 
 arr.each do |num|
-    if num == number
-      puts "The number #{number} appears in the array"
-    end
-  end
+  puts "The number #{number} appears in the array" if num == number
+end
