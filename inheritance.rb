a7e7df2791338aa026c57e7e515d8fd4d0291940
jarencudilla <jarencudilla@gmail.com>
2021-05-04 21:53:28 +0800

    Create inheritance.rb
    inheritance lesson

---
+++ inheritance.rb
@@ -0,0 +1,92 @@
+# class Animal
+#     def speak
+#         "Hello!"
+#     end
+# end
+
+# class GoodDog < Animal
+# end
+
+# sparky = GoodDog.new
+# puts sparky.speak
+
+
+# class Animal
+#   def speak
+#       "Hello!"
+#   end
+# end
+
+# class GoodDog < Animal
+#   def speak
+#     # super + 
+#     " from GoodDog Class"
+#   end
+# end
+
+# sparky = GoodDog.new
+# puts sparky.speak
+
+
+# module Swimmable
+#   def swim
+#     "I'm Swimming!"
+#   end
+# end
+
+# class Animal; end
+
+# class Fish < Animal
+#   include Swimmable
+# end
+
+# dory = Fish.new
+# puts dory.swim
+
+
+# namespacing 
+
+# module Language
+#   class Ruby
+#     def info
+#       puts "Ruby Programming Language"
+#     end
+#   end
+# end
+
+# ruby = Language::Ruby.new
+# ruby.info
+
+# private method
+
+# class Person
+#   def initialize(age)
+#     @age = age
+#   end
+# end
+
+
+# protected_methods
+
+class Animal
+  def initialize(age)
+    @age = age
+  end
+  # def less_than_age(animal)
+  def <(animal)
+    age < animal.age
+  end
+
+protected
+
+  def age 
+    @age
+  end
+end
+
+fido = Animal.new(3)
+cat =  Animal.new(2)
+
+puts fido < cat
+# puts fido.less_than_age(cat)
+
