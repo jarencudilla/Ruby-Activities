2a401500148f20c89f42d46fd2f0d48772f855fd
jarencudilla <jarencudilla@gmail.com>
2021-05-04 21:50:41 +0800

    Update square.rb
    from
    
    test_arr =[-1, 0, 3, 4, 25, 26]
    def isSquare(num)
        i = 0
        return false if num < 0
        (0..num).each do
            return true if (i*i == num)
            i += 1
            return false if (i*i > num)
        end
    end
    test_arr.each {|x| puts "#{x} => #{isSquare(x)}"}

--- square.rb
+++ square.rb
@@ -1,13 +1,17 @@
-test_arr =[-1, 0, 3, 4, 25, 26]
+# frozen_string_literal: true
+
+test_arr = [-1, 0, 3, 4, 25, 26]
 
 def isSquare(num)
-    i = 0
-    return false if num < 0
-    (0..num).each do
-        return true if (i*i == num)
-        i += 1
-        return false if (i*i > num)
-    end
+  i = 0
+  return false if num.negative?
+
+  (0..num).each do
+    return true if i * i == num
+
+    i += 1
+    return false if i * i > num
+  end
 end
 
-test_arr.each {|x| puts "#{x} => #{isSquare(x)}"}
+test_arr.each { |x| puts "#{x} => #{isSquare(x)}" }
