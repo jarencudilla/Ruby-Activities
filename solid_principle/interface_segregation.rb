5c080afe3f066820df1c31b85ddf7d9b2576bf69
jarencudilla <jarencudilla@gmail.com>
2021-05-09 22:56:12 +0800

    Update interface_segregation.rb

--- solid_principle/interface_segregation.rb
+++ solid_principle/interface_segregation.rb
@@ -2,6 +2,76 @@
 
 #bad code
 
+class GunStore
+       def initialize(gun_type, order, quantity, price)
+           @gun_type = gun_type
+           @order = order
+           @quantity = quantity
+           @price = price    
+       end
+       
+       def purchase_total
+          @quantity * @price
+       end
+       
+       def select_gun_type(type)
+           case gun_type(type)
+           when "pistols"
+               pistols
+           when "shotguns"
+               shotguns
+           when "assault_rifles"
+               assault_rifles
+           when "rifle"
+               rifles
+           else  
+               raise NotInStock, "We're out of #{gun_type(type)}"  
+           end
+       end
+   
+   private
+   
+       def process_order
+       end
+   
+       def check_inventory
+       end
+   
+       def check_gun_license
+       end
+end
 
+#good code?
 
-#good code? 
+class GunStore
+    def initialize(gun_type, order, quantity, price)
+        @gun_type = gun_type
+        @order = order
+        @quantity = quantity
+        @price = price    
+    end
+    def check_price
+    end
+    def calculate_price
+    end
+    
+    def buy_guns
+        check_inventory
+        check_price
+        check_gun_license
+         
+        process_order
+        calculate_price
+    end
+
+private
+
+    def process_order
+    end
+
+    def check_inventory
+    end
+
+    def check_gun_license
+    end
+end
